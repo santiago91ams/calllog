96aec1265f29b2298cf52f4851e6ab971c514cb4
santiago91ams <contact.istrate@gmail.com>
2017-07-31 21:54:53 +0300

    *query call logs

--- Calllog/app/src/main/java/com/service/calllog/MainActivity.java
+++ Calllog/app/src/main/java/com/service/calllog/MainActivity.java
@@ -3,10 +3,18 @@ package com.service.calllog;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.provider.CallLog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        getCallDetails();
     }
 
     private void updateService(String label, boolean serviceState) {
@@ -57,5 +67,51 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return false;
+    }
+
+    public long convertDateToMilis(Date date) {
+        return date.getTime();
+    }
+
+    private void getCallDetails() {
+
+        StringBuffer sb = new StringBuffer();
+        Cursor managedCursor = managedQuery(CallLog.Calls.CONTENT_URI, null, null, null, android.provider.CallLog.Calls.DATE + " DESC limit 1;");
+//        Cursor managedCursor = managedQuery(CallLog.Calls.CONTENT_URI, null, null, null, null);
+        int number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
+        int type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
+        int date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
+        int duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION);
+        int id = managedCursor.getColumnIndex(CallLog.Calls._ID);
+        sb.append("Call Details :");
+        while (managedCursor.moveToNext()) {
+            String phNumber = managedCursor.getString(number);
+            String callType = managedCursor.getString(type);
+            String callDate = managedCursor.getString(date);
+            Date callDayTime = new Date(Long.valueOf(callDate));
+            String callDuration = managedCursor.getString(duration);
+            String idul = managedCursor.getString(id);
+            String dir = null;
+            int dircode = Integer.parseInt(callType);
+            switch (dircode) {
+                case CallLog.Calls.OUTGOING_TYPE:
+                    dir = "OUTGOING";
+                    break;
+
+                case CallLog.Calls.INCOMING_TYPE:
+                    dir = "INCOMING";
+                    break;
+
+                case CallLog.Calls.MISSED_TYPE:
+                    dir = "MISSED";
+                    break;
+            }
+            sb.append("\nPhone Number:--- " + phNumber + " \nCall Type:--- " + dir +
+                    " \nCall Date:--- " + convertDateToMilis(callDayTime) + " \nCall duration in sec :--- " + callDuration +
+                    " \nCall ID:--- " + idul);
+            sb.append("\n----------------------------------");
+        }
+        managedCursor.close();
+        ((TextView) findViewById(R.id.call)).setText(sb);
     }
 }
---
+++ Calllog/app/src/main/java/com/service/calllog/SplashScreen.java
@@ -0,0 +1,8 @@
+package com.service.calllog;
+
+/**
+ * Created by Ghita on 31/07/2017.
+ */
+
+public class SplashScreen {
+}
